522fea77f00240ac0dfeaf1c39a5c85d7dda0146
Balasankar C <balasankarc@autistici.org>
2015-11-09 12:26:20 +0530

    Basic working model

--- app/views.py
+++ app/views.py
@@ -1,11 +1,31 @@
 from app import app
 from flask import render_template, request
+import json
+import os
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/get_dependency')
-def get_dependency():
-    return request.args['gemname']
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        apps = request.form.getlist('appname')
+        gemname = request.form.get('gemname')
+        gems = {}
+        flag = 0
+        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+        for app in apps:
+            appname = app + "_debian_status.json"
+            filepath = os.path.join(SITE_ROOT, "static", appname)
+            inputfile = open(filepath)
+            filecontent = inputfile.read()
+            inputfile.close()
+            deps = json.loads(filecontent)
+            gem = [x for x in deps if x['name'] == gemname]
+            if gem:
+                flag = 1
+            gems[app] = gem
+        return render_template('index.html',
+                               gemname=gemname,
+                               gemlist=gems,
+                               flag=flag)
