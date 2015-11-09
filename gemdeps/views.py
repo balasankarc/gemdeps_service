b544224e02427e626d28eac0438e4162cab87eb7
Balasankar C <balasankarc@autistici.org>
2015-11-09 22:57:42 +0530

    Typeahead textboxes

--- gemdeps/views.py
+++ gemdeps/views.py
@@ -1,31 +1,41 @@
 from gemdeps import app
-from flask import render_template, request
+from flask import render_template, request, Markup
 import json
 import os
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    completedeplist = {}
+    gemnames = "["
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    for app in ['diaspora', 'gitlab', 'asciinema']:
+        appname = app + "_debian_status.json"
+        filepath = os.path.join(SITE_ROOT, "static", appname)
+        inputfile = open(filepath)
+        filecontent = inputfile.read()
+        inputfile.close()
+        deps = json.loads(filecontent)
+        completedeplist[app] = deps
+        gemnames += ", ".join([str('"' + x['name'] + '"') for x in deps])
+        gemnames += ", "
+    gemnames += "]"
+    gemnames = Markup(gemnames)
+    print completedeplist
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('index.html', gemnames=gemnames)
     else:
         apps = request.form.getlist('appname')
         gemname = request.form.get('gemname')
         gems = {}
         flag = 0
-        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
         for app in apps:
-            appname = app + "_debian_status.json"
-            filepath = os.path.join(SITE_ROOT, "static", appname)
-            inputfile = open(filepath)
-            filecontent = inputfile.read()
-            inputfile.close()
-            deps = json.loads(filecontent)
-            gem = [x for x in deps if x['name'] == gemname]
+            gem = [x for x in completedeplist[app] if x['name'] == gemname]
             if gem:
                 flag = 1
             gems[app] = gem
         return render_template('index.html',
+                               gemnames=gemnames,
                                gemname=gemname,
                                gemlist=gems,
                                flag=flag)
