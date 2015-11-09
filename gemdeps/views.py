66b7b79891aaf486d2490b9298643b3f7417f9c4
Balasankar C <balasankarc@autistici.org>
2015-11-10 00:35:23 +0530

    Integrate progressbar generation

--- gemdeps/views.py
+++ gemdeps/views.py
@@ -1,7 +1,9 @@
-from gemdeps import app
-from flask import render_template, request, Markup
 import json
 import os
+
+from flask import Markup, render_template, request
+
+from gemdeps import app
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -39,3 +41,36 @@ def index():
                                gemname=gemname,
                                gemlist=gems,
                                flag=flag)
+
+
+@app.route('/status/<appname>')
+def status(appname):
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    appfilename = appname + "_debian_status.json"
+    filepath = os.path.join(SITE_ROOT, "static", appfilename)
+    inputfile = open(filepath)
+    filecontent = inputfile.read()
+    inputfile.close()
+    deps = json.loads(filecontent)
+    packaged_count = 0
+    unpackaged_count = 0
+    itp_count = 0
+    total = 0
+    for n in deps:
+        if n['status'] == 'Packaged' or n['status'] == 'NEW':
+            packaged_count += 1
+        elif n['status'] == 'ITP':
+            itp_count += 1
+        else:
+            unpackaged_count += 1
+    total = len(deps)
+    percent_complete = (packaged_count * 100) / total
+    return render_template('status.html',
+                           appname=appname.title(),
+                           deps=deps,
+                           packaged_count=packaged_count,
+                           unpackaged_count=unpackaged_count,
+                           itp_count=itp_count,
+                           total=total,
+                           percent_complete=percent_complete
+                           )
